35105d4706c6c5060a6ed5d3c59656bf389a983f
CookieDSEU <dpdp2524@gmail.com>
2016-08-27 18:49:35 +0800

    头像上传逻辑

--- app/src/main/java/com/plantnurse/plantnurse/utils/Constants.java
+++ app/src/main/java/com/plantnurse/plantnurse/utils/Constants.java
@@ -9,4 +9,5 @@ public class Constants {
     public static final String SIGNIN_URL="http://www.plantnurse.top/login.php";
     public static final String SIGNUP_URL="http://www.plantnurse.top/signup.php";
     public static final String AVATAR_URL="http://www.plantnurse.top/avatar/avatar.php";
+    public static final String AVATAR_UPLOAD_URL="http://www.plantnurse.top/avatar/upload.php";
 }
