019930d9fdae2ff7595ec1fbbded372d04eb968b
YzcSeu <794614382@qq.com>
2016-08-29 11:41:06 +0800

    Merge remote-tracking branch 'upstream/master'

---
+++ app/src/main/java/com/plantnurse/plantnurse/Activity/Resetcity.java
@@ -0,0 +1,15 @@
+package com.plantnurse.plantnurse.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.plantnurse.plantnurse.R;
+
+public class Resetcity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_city);
+    }
+}
