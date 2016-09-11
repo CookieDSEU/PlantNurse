3b8a9e22027f4d2976794e04097ec9d21a3e3294
EasonTao <295829650@qq.com>
2016-09-11 09:06:31 +0800

    自定义植物floatingbutton 植物已样日期计算bug

--- app/src/main/java/com/plantnurse/plantnurse/Activity/MyPlantActivity.java
+++ app/src/main/java/com/plantnurse/plantnurse/Activity/MyPlantActivity.java
@@ -182,6 +182,9 @@ public class MyPlantActivity extends KSimpleBaseActivityImpl implements IBaseAct
 
 
                 birthday = DataManager.getMyPlant().response.get(selectedId).birthday+"";
+                int birth = Integer.parseInt(birthday);
+                birth -= 100;
+                birthday = birth+"";
                 Calendar c = Calendar.getInstance();
                 int nowyear =c.get(Calendar.YEAR);
                 int nowmonth = c.get(Calendar.MONTH);
